572aed8a3160276ef0fa1fd26078a8a2146940dd
gather <chenruiqiang@yd-x.com>
2019-10-12 16:43:45 +0800

    进销存系统本地运行;

---
+++ application/Setting.php
@@ -0,0 +1,18 @@
+<?php
+// 佛山市三叶草IT工作室 技术支持
+// www.sycit.cn
+// 更新时间：2019-10-12 15:17:00
+return array(
+  'syc_basehost' => '',
+  'syc_webname' => '北京方兴未艾科技有限公司',
+  'syc_webtel' => '',
+  'syc_webfax' => '',
+  'syc_tousu' => '',
+  'syc_webemail' => 'chenruiqiang@yd-x.com',
+  'syc_webqq' => '791852943',
+  'syc_address' => '北京市顺义区',
+  'syc_keywords' => '北京方兴未艾科技有限公司',
+  'syc_description' => '北京方兴未艾科技有限公司',
+  'syc_powerby' => 'Copyright © 2017-2020 北京方兴未艾科技有限公司 版权所有',
+  'syc_beian' => '',
+);
